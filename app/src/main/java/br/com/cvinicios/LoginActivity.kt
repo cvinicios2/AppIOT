8db4d714f7f30371d98e004b903876237f064fa1
Logon Aluno <logonrmlocal@fiap.com.br>
2019-06-08 12:37:22 -0300

    continuacao

--- app/src/main/java/br/com/cvinicios/LoginActivity.kt
+++ app/src/main/java/br/com/cvinicios/LoginActivity.kt
@@ -1,12 +1,70 @@
 package br.com.cvinicios
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        if (usuarioLogado()){
+            proximaTelaLogada()
+        }
+
+        botaoEntrar.setOnClickListener{
+            validarUsuario()
+        }
+    }
+
+    private fun usuarioLogado(): Boolean{
+        return mAuth.currentUser != null
+    }
+
+    private fun proximaTelaLogada(){
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    private fun validarUsuario(){
+        val email = inputEmail.editText?.text.toString()
+        val senha = inputSenha.editText?.text.toString()
+
+        if(email.isEmpty()){
+            inputEmail.isErrorEnabled = true
+            inputEmail.error = "O e-mail deve ser preenchido"
+            return
+        }else{
+            inputEmail.isErrorEnabled = false
+        }
+
+        if(senha.isEmpty()){
+            inputSenha.isErrorEnabled = true
+            inputSenha.error = "A senha deve ser preenchida"
+            return
+        }else{
+            inputSenha.isErrorEnabled = false
+        }
+
+        mAuth.signInWithEmailAndPassword(
+            inputEmail.editText?.text.toString(),
+            inputSenha.editText?.text.toString())
+            .addOnCompleteListener(this){ task ->
+                if (task.isSuccessful){
+                    proximaTelaLogada()
+                }else{
+                    Toast.makeText(this, "Falha de autenticação",
+                        Toast.LENGTH_LONG).show()
+                }
+        }
     }
 }
